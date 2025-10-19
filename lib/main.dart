368155856417d66fc170ce4ad59252f4e2aa69b4
Mohammad Mahdi Alizadgan <mmailzadgann@gmail.com>
2025-10-19 11:33:01 +0330

    create folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nftmarketplace/presentation/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
      
-      home: ,
+      home: OnboardingScreen(),
     );
   }
 }
