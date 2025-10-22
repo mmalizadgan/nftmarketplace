ea345282e76983d6b8bf65c26fbaf03be4cfde02
Mohammad Mahdi Alizadgan <mmailzadgann@gmail.com>
2025-10-22 17:04:02 +0330

    add color

--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nftmarketplace/core/constants/app_colors.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -7,29 +8,68 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 99.1.h,
-            left: -752.8.w,
-            child: Image.asset(
-              'assets/images/bg.jpg',
-              width: 1428.8824462890625.w,
-              height: 1085.61572265625.h,
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          // تنظیم موقعیت عکس
           ),
-          Positioned(
-            width: 299.107421875.w,
-            height: 86.4888916015625.h,
-            top: 80.18.h,
-            left: 26.13.w,
-            child: Text(
-              'Welcome to\nNFT Marketplace',
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 35.sp),
+        ),
+        child: Stack(
+          children: [
+            // لایه 1: گرادیانت اول (139.87deg)
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      AppColors.gradientStart.withOpacity(0.5),
+                      AppColors.gradientEnd.withOpacity(0.5),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            
+            // لایه 2: گرادیانت دوم (180deg)
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      AppColors.gradient180Start.withOpacity(0.20),
+                      AppColors.gradient180End.withOpacity(0.20),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            
+            // لایه 3: سفید شفاف
+            Positioned.fill(
+              child: Container(
+                color: AppColors.whiteOverlay.withOpacity(0.2),
+              ),
+            ),
+            
+            // // محتوا
+            // SafeArea(
+             
+         
+                    
+                  
+                  
+           
+            // ),
+          ],
+        ),
       ),
     );
   }
